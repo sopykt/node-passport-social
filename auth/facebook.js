ebbb8f652f459676d4a45fe1734cbf170a4bd47a
soepaing <sopykt09@gmail.com>
2018-02-15 18:44:46 +0630

    localhost to heroku url

--- auth/facebook.js
+++ auth/facebook.js
@@ -5,7 +5,7 @@ var User = require('../models/User');
 passport.use(new FacebookStrategy({
     clientID: "206871776530963",
     clientSecret: "cd84196e7e743fa6049055f832f54cb5",
-    callbackURL: "http://127.0.0.1:3000/auth/facebook/callback"
+    callbackURL: "https://my-node-passport-social.herokuapp.com/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     User.findOrCreate({name: profile.displayName}, {name: profile.displayName,userid: profile.id}, function(err, user) {
