edd6e7da052ac3108ef4c36ca1d8e7a45b36b5bc
soepaing <sopykt09@gmail.com>
2018-02-15 18:58:51 +0630

    heroku url to custom domain

--- auth/facebook.js
+++ auth/facebook.js
@@ -5,7 +5,7 @@ var User = require('../models/User');
 passport.use(new FacebookStrategy({
     clientID: "206871776530963",
     clientSecret: "cd84196e7e743fa6049055f832f54cb5",
-    callbackURL: "https://my-node-passport-social.herokuapp.com/auth/facebook/callback"
+    callbackURL: "https://social-login.formyanmar.tk/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     User.findOrCreate({name: profile.displayName}, {name: profile.displayName,userid: profile.id}, function(err, user) {
